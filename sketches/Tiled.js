177ff95b9d7138b73e2c9a2d338df641a25d10dd
Dan Garden <dan@Dans-MacBook-Air.local>
2019-05-11 11:50:06 +1000

    firesss

--- sketches/Tiled.js
+++ sketches/Tiled.js
@@ -4,11 +4,9 @@ const tiled = new Canv('canvas', {
     setup() {
         this.pixels = false;
         this.every = 2;
-        this.size = 5;
-        const img = new Pic("images/test.jpg", 0, 0, this.width, this.height, img => {
+        this.size = 10;
+        const img = new Pic("images/test.jpg", 0, 0, this.widthgit, this.height, img => {
             this.add(img);
-            // this.pixels = this.getPixels();
-            // this.invert();
         });
     },
 
@@ -18,7 +16,7 @@ const tiled = new Canv('canvas', {
             this.filterPixels((color, x, y) => {
                 if(x < this.mouseX+this.size && x > this.mouseX-this.size &&
                     y < this.mouseY+this.size && y > this.mouseY-this.size) {
-                    return color.invert();
+                    return color.shade(255);
                 } else {
                     return color;
                 }
