699c32f9f2539e95e9db3996d46874196587fdbb
Dan Garden <dan@Dans-MacBook-Air.local>
2019-05-23 19:15:59 +1000

    modified graph

--- sketches/Graph.js
+++ sketches/Graph.js
@@ -24,6 +24,40 @@ new Canv('canvas', {
         });
     },
     draw() {
-        this.add(this.graph);
+        this.clear();
+        const bounds = new Rect(0, 10, this.width, this.height-10);
+
+        bounds.noFill();
+        this.add(bounds);
+
+        const highest = Math.max(...this.data.fields.map(f => f.number));
+        const len = this.data.fields.length;
+        this.data.fields = this.data.fields.map((field, i) => {
+            let g = 20;
+            let w = (bounds.width / len) - g;
+            let h;
+            if(field.animating && field.animating < (field.number) * (bounds.height / highest)) {
+                field.animating += this.data.inc;
+                h = field.animating;
+            } else {
+                h = field.number * (bounds.height / highest);
+            }
+            let x = (g / 4) + bounds.x + (i * (w + g));
+            let y = (bounds.height + bounds.y) - h;
+            let c = field.color;
+            const s = 10;
+            const bar = new ShapeGroup({
+                shadow: new Rect(x+s, y-s, w, h+s).setColor(c.shade(-100)),
+                bar: new Rect(x, y, w, h).setColor(c)
+            });
+
+            if(bar.contains(this.mouseX, this.mouseY)) {
+                bar.bar.setColor(c.shade(20));
+            } else {
+                bar.bar.setColor(c.shade(0));
+            }
+            this.add(bar);
+            return field;
+        });
     }
 })
