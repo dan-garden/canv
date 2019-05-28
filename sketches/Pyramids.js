4d3980815fcfb20858dd8e87c4f7336a48726cf1
Daniel Garden <daniel.garden@bundall.secureclick.com.au>
2019-05-28 16:43:42 +1000

    Create Pyramids

--- sketches/Pyramids.js
+++ sketches/Pyramids.js
@@ -1,9 +1,16 @@
 new Canv('canvas', {
     setup() {
-        this.pyramid = new ShapeGroup();
+        this.pyramid = new ShapeGroup({
+            casing_outer: new Triangle(),
+            casing_inner: new Triangle(),
+
+            chambers: new ShapeGroup({
+                
+            })
+        });
     },
 
     draw() {
-        
+        this.add(this.pyramid);
     }
 })
