965162c5ed4314bb154c13ae30f08de250ac9366
Daniel Garden <dangarden@gmail.com>
2021-04-23 17:37:56 +1000

    MORE CHANGES, points and colours

--- src/canv.js
+++ src/canv.js
@@ -52,9 +52,7 @@ class Microphone {
         window.AudioContext = window.AudioContext || window.webkitAudioContext;
         navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
         // now just wait until the microphone is fired up
-        window.onload = () => {
-            this.init();
-        }
+        this.init();
     }
 
     init() {
@@ -825,7 +823,7 @@ class Vector {
         for (let i = 0; i <= count; ++i) {
             points.push(new Vector(a.x + i * d * Math.cos(fi), a.y + i * d * Math.sin(fi)));
         }
-        return points;
+        return new VectorList(points);
     }
 
     setPos(x, y) {
@@ -1608,6 +1606,7 @@ class Rect extends Shape {
 
     intersects(shape) {
         if (shape instanceof Rect) {
+            const rectB = shape;
             var intersectTop = Math.max(this.y, rectB.y);
             var intersectRight = Math.min(this.x2, rectB.x2);
             var intersectBottom = Math.min(this.y2, rectB.y2);
@@ -1782,12 +1781,12 @@ class Circle extends Shape {
         return ((x - this.x) * (x - this.x) + (y - this.y) * (y - this.y) <= this.radius * this.radius);
     }
 
-    getPoints(count=10) {
+    getPoints(count=10, angle=0) {
         const points = [];
 
         for(let i = 0; i < count; i++) {
-            const g = Math.round(360 / count) * i;
-            const a = g * (Math.PI/180);
+            const g = (Math.round(360 / count) * i) + angle;
+            const a = (g * (Math.PI/180));
             const x = this.x + this.radius * Math.cos(a);
             const y = this.y + this.radius * Math.sin(a);
     
@@ -1795,7 +1794,7 @@ class Circle extends Shape {
             points.push(vector);
         }
 
-        return points;
+        return new VectorList(points);
     }
 
     intersects(shape) {
@@ -1997,6 +1996,10 @@ class Grid extends ShapeGroup {
         this.cols = cols;
         const cw = width / this.cols;
         const ch = height / this.rows;
+        this.width = width;
+        this.height = height;
+        this.x = x;
+        this.y = y;
         const white = new Color(255);
         const black = new Color(0);
         for (let i = 0; i < this.rows * this.cols; i++) {
@@ -2134,6 +2137,61 @@ class Text extends Shape {
     }
 }
 
+
+class VectorList extends Shape {
+    constructor(points=[]) {
+        super();
+        this.points = points;
+        this.size = 1;
+    }
+
+    intersects(shape) {
+        const intersects = [];
+        const tPoint = new Circle(0, 0, this.size);
+        this.points.forEach(point => {
+            tPoint.x = point.x;
+            tPoint.y = point.y;
+            if(tPoint.intersects(shape)) {
+                intersects.push(new Vector(point.x, point.y));
+            }
+        });
+
+        return intersects.length ? intersects : false;
+    }
+
+    render(canv) {
+        const tPoint = new Circle(0, 0, this.size);
+        tPoint.setColor(this.color);
+        this.points.forEach(point => {
+            tPoint.x = point.x;
+            tPoint.y = point.y;
+            tPoint.render(canv);
+        });
+    }
+    
+    add(vector) {
+        this.points.push(vector);
+    }
+
+    get length() {
+        return this.points.length;
+    }
+
+    join(vectorList) {
+        const lineGroup = new ShapeGroup();
+        this.points.forEach((point, index) => {
+            if(vectorList.points[index]) {
+                const start = this.points[index];
+                const end = vectorList.points[index];
+
+                lineGroup.add(new Line(start, end));
+            }
+        })
+
+        return lineGroup;
+    }
+}
+
 class Canv {
     constructor(selector, config) {
         let noSelector = true;
@@ -2162,6 +2220,7 @@ class Canv {
 
 
         this.keysDown = {};
+        this.keysUp = {};
 
         this.width = 100;
         this.height = 100;
@@ -2407,6 +2466,7 @@ class Canv {
 
         window.addEventListener("keyup", e => {
             if (this.keysDown[e.key]) {
+                this.keysUp[e.key] = true;
                 delete this.keysDown[e.key];
             }
         });
@@ -2457,19 +2517,19 @@ class Canv {
         }, false);
 
         document.body.addEventListener("touchstart", (e) => {
-            if (this.fullscreen && this.isMobile() && e.target == this.canvas) {
+            if (this.fullscreen && this.isMobile && e.target == this.canvas) {
                 e.preventDefault();
             }
         }, false);
 
         document.body.addEventListener("touchend", (e) => {
-            if (this.fullscreen && this.isMobile() && e.target == this.canvas) {
+            if (this.fullscreen && this.isMobile && e.target == this.canvas) {
                 e.preventDefault();
             }
         }, false);
 
         document.body.addEventListener("touchmove", (e) => {
-            if (this.fullscreen && this.isMobile() && e.target == this.canvas) {
+            if (this.fullscreen && this.isMobile && e.target == this.canvas) {
                 e.preventDefault();
             }
         }, false);
@@ -2504,6 +2564,10 @@ class Canv {
         return this.keysDown[key];
     }
 
+    keyUp(key) {
+        return this.keysUp[key];
+    }
+
     pressKey(keys, hold = 10) {
         if (typeof keys === "string") {
             keys = [keys];
@@ -2514,6 +2578,7 @@ class Canv {
                 this.keysDown[key] = true;
                 setTimeout(() => {
                     if (this.keysDown[key]) {
+                        this.keysUp[key] = true;
                         delete this.keysDown[key];
                     }
                 }, hold);
@@ -2567,6 +2632,8 @@ class Canv {
 
             this.canvas.style.cursor = this.cursor;
             this.cursor = "default";
+
+            this.keysUp = {};
 
             requestAnimationFrame(this.$loop.bind(this));
         }
@@ -2768,7 +2835,7 @@ class Canv {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
-    static isMobile() {
+    static get isMobile() {
         return (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) ||
             /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0, 4)))
     }
